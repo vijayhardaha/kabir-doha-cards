1ef01e8570c43ca5e4836032c6aa1268fea8de72
Vijay Hardaha <vijayhardaha@gmail.com>
2024-09-07 18:48:01 +0530

    chore(.eslint): ignore jsx and global unknown-property rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-// .eslintrc.js
 module.exports = {
 	root: true,
 	extends: [
@@ -19,11 +18,23 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 2024, // Set to latest ECMAScript version.
 		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true, // Enable JSX syntax.
+		},
 	},
 	rules: {
 		// Customize ESLint rules here
 		"react/react-in-jsx-scope": "off", // Next.js does not require importing React in JSX files.
 		"prettier/prettier": "error", // Enforce Prettier formatting as errors.
+		"react/no-unknown-property": [
+			"error",
+			{
+				ignore: [
+					"jsx", // Ignore the 'jsx' property in JSX.
+					"global", // Ignore the 'global' property in JSX.
+				],
+			},
+		],
 		"import/order": [
 			"error",
 			{
