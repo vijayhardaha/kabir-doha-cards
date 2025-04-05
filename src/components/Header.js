7c5376ef184472cb76b28ff48f9e84cefa993fe8
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-05 14:42:20 +0530

    refactor: adjust header styles and update button labels for consistency

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,11 @@ import { getShareUrl } from "@/utils/share";
  * @returns {JSX.Element} The rendered header component.
  */
 const Header = () => (
-  <header className="mb-8 py-4">
+  <header className="mb-8 py-2">
     <div className="container mx-auto">
       <div className="flex items-center justify-between">
         <h1 className="flex items-center gap-2 font-bold text-stone-900">
-          <Logo aria-hidden="true" className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16" />
+          <Logo aria-hidden="true" className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12" />
 
           <Link href="/" className="text-lg md:text-xl lg:text-2xl">
             Kabir Doha Cards
@@ -31,7 +31,7 @@ const Header = () => (
             aria-label="View the source code on GitHub"
           >
             <AiOutlineGithub aria-hidden="true" className="mr-1 text-sm" />
-            View Source Code
+            Source Code
           </a>
 
           {/* Vertical Divider */}
@@ -45,7 +45,7 @@ const Header = () => (
             aria-label="Share on WhatsApp"
           >
             <AiOutlineWhatsApp aria-hidden="true" className="mr-1 text-sm" />
-            Share the App
+            Share It!
           </a>
         </div>
       </div>
