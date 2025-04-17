2173488f907a6b7008639f632bd10d26a3466010
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-17 11:36:27 +0530

    chore: update ESLint configuration and dependencies
    - Added new ESLint plugins: @eslint/compat, @eslint/eslintrc, @eslint/js, and globals.
    - Upgraded ESLint to version 9.24.0.
    - Created a new ESLint configuration file (eslint.config.mjs) with a comprehensive setup including React, JSX accessibility, and Prettier integration.
    - Configured global ignores for various directories and files.
    - Defined rules for import ordering and React properties.

---
+++ eslint.config.mjs
@@ -0,0 +1,114 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores([
+    "**/node_modules/",
+    "**/build/",
+    "**/dist/",
+    "**/coverage/",
+    "**/.env",
+    "**/.env.local",
+    "**/*.log",
+    "**/bower_components/",
+    "**/*.tsbuildinfo",
+    "test/**/*.js",
+    "spec/**/*.js",
+    "**/*.generated.js",
+  ]),
+  {
+    extends: compat.extends(
+      "next",
+      "next/core-web-vitals",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:prettier/recommended"
+    ),
+
+    plugins: {
+      react,
+      "jsx-a11y": jsxA11Y,
+      importPlugin: fixupPluginRules(importPlugin),
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: babelParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": "error",
+
+      "react/no-unknown-property": [
+        "error",
+        {
+          ignore: ["jsx", "global"],
+        },
+      ],
+
+      "importPlugin/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal"],
+
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["react"],
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+
+          "newlines-between": "always",
+          warnOnUnassignedImports: true,
+        },
+      ],
+    },
+  },
+]);
