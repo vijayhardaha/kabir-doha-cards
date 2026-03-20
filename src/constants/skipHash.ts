af51fe3a9b5483bdc320996766044cf76d8effff
Vijay Hardaha <vijayhardaha@proton.me>
2026-03-20 13:08:53 +0530

    feat: remove unuses hash util

--- src/constants/skipHash.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Secret hash value for skipping rate limiting
- * In production, this should be stored in environment variables
- */
-export const BYPASS_RATE_LIMIT_HASH = 'r>9c%sW9Pd`vaQ3n^Jn_ApP>1.xgwP50.=i]Dym823B^;nID';
